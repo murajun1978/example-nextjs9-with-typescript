e6080871db23973e40159a07435237ece12959f9
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2019-07-22 00:44:26 +0900

    Add Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // setupFiles: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest'
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx']
+};
---
+++ pages/__tests__/index.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@testing-library/react/cleanup-after-each';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+
+import Home from '../';
+
+describe('Home', () => {
+  test('Display message', () => {
+    const { getByText } = render(<Home />);
+    expect(getByText('Hello Next.js 9')).toBeInTheDocument();
+  });
+});
