77a6b5799709a4cd2fc164c82aa034af814a69f6
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2019-07-22 01:05:52 +0900

    Fix setting of Jest
    - Remove babel-jest
    - Remove transform form jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // setupFiles: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.tsx?$': 'babel-jest'
-  },
+  // transform: {
+  //   '^.+\\.tsx?$': 'babel-jest'
+  // },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx']
 };
